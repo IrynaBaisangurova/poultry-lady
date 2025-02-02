02356a8d69462860edfec1aad0930cfef55a169b
Iryna <laudaeren@ukr.net>
2025-02-02 18:32:55 +0200

    change

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -1,36 +1,36 @@
 
 
-// const items = document.querySelectorAll(".accordion button");
-
-// function toggleAccordion() {
-//   const itemToggle = this.getAttribute('aria-expanded');
-  
-//   for (i = 0; i < items.length; i++) {
-//     items[i].setAttribute('aria-expanded', 'false');
-//   }
-  
-//   if (itemToggle == 'false') {
-//     this.setAttribute('aria-expanded', 'true');
-//   }
-// }
-
-// items.forEach(item => item.addEventListener('click', toggleAccordion));
-
 const items = document.querySelectorAll(".accordion button");
 
 function toggleAccordion() {
-  const content = this.nextElementSibling; 
-  const itemToggle = this.getAttribute("aria-expanded");
-
-  items.forEach((item) => {
-    item.setAttribute("aria-expanded", "false");
-    item.nextElementSibling.style.maxHeight = null; 
-  });
-
-  if (itemToggle === "false") {
-    this.setAttribute("aria-expanded", "true");
-    content.style.maxHeight = content.scrollHeight + "px"; 
+  const itemToggle = this.getAttribute('aria-expanded');
+  
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute('aria-expanded', 'false');
+  }
+  
+  if (itemToggle == 'false') {
+    this.setAttribute('aria-expanded', 'true');
   }
 }
 
-items.forEach((item) => item.addEventListener("click", toggleAccordion));
+items.forEach(item => item.addEventListener('click', toggleAccordion));
+
+// const items = document.querySelectorAll(".accordion button");
+
+// function toggleAccordion() {
+//   const content = this.nextElementSibling; 
+//   const itemToggle = this.getAttribute("aria-expanded");
+
+//   items.forEach((item) => {
+//     item.setAttribute("aria-expanded", "false");
+//     item.nextElementSibling.style.maxHeight = null; 
+//   });
+
+//   if (itemToggle === "false") {
+//     this.setAttribute("aria-expanded", "true");
+//     content.style.maxHeight = content.scrollHeight + "px"; 
+//   }
+// }
+
+// items.forEach((item) => item.addEventListener("click", toggleAccordion));
